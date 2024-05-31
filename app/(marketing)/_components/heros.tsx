b1b2a8e729120b4b29eadf3231a8e7ef93045230
spurs0929 <briankang0929@gmail.com>
2024-05-31 22:57:35 +0800

    feat: finished landing page

--- app/(marketing)/_components/heros.tsx
+++ app/(marketing)/_components/heros.tsx
@@ -11,7 +11,13 @@ export const Heros = () => {
           <Image
             src="/documents.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Documents"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Documents"
           />
         </div>
@@ -19,7 +25,13 @@ export const Heros = () => {
           <Image
             src="/reading.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Reading"
           />
         </div>
